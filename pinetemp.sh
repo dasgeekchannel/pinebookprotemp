dbbf15ebad64fdc5779fd0932f90d21f12ca5517
Nan-Hung Hsieh <25087463+nanhung@users.noreply.github.com>
2020-06-29 12:03:42 -0500

    Update pinetemp.sh
    The definition of rkbat seems unnecessary...

--- pinetemp.sh
+++ pinetemp.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# Purpose: Display the  temperature of Pinebook Pro 
+# Purpose: Display the temperature of Pinebook Pro
 # Modified version of script by Vivek Gite. Modified for Pinebook Pro by DasGeek
 # -------------------------------------------------------
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 gpu=$(</sys/class/thermal/thermal_zone1/temp)
-rkbat=$(</sys/class/thermal/thermal_zone2/temp)
 
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
 answer= echo "CPU => $((cpu/1000))'C"
 answer= echo "GPU => $((gpu/1000))'C"
-
